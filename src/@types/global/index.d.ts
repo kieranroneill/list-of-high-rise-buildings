e2396661ae616dde2d7a4e67212dbde419d29a81
Kieran O'Neill <kieranroneill@googlemail.com>
2018-10-15 13:07:44 +0100

    Converts city into common type

---
+++ src/@types/global/index.d.ts
@@ -0,0 +1,12 @@
+interface City {
+    '100m+': number;
+    '150m+': number;
+    '200m+': number;
+    '300m+': number;
+    allBuildings: number;
+    allStructures: number;
+    country: string;
+    id: number;
+    name: string;
+    telecomTowers: number;
+}
--- src/api/routes/CityRoute.ts
+++ src/api/routes/CityRoute.ts
@@ -28,8 +28,20 @@ export class CityRoute extends BaseRoute {
     private async getCities(request: Request, response: Response, next: NextFunction): Promise<void> {
         try {
             const data: any[] = await csv().fromFile(join(__dirname, '..', 'data', 'cities.csv'));
+            const cities: City[] = data.map((data: any) => ({
+                '100m+': parseInt(data['100m+']),
+                '150m+': parseInt(data['150m+']),
+                '200m+': parseInt(data['200m+']),
+                '300m+': parseInt(data['300m+']),
+                allBuildings: parseInt(data['All\nBuildings']),
+                allStructures: parseInt(data['All\nStructures']),
+                country: data['Country'],
+                id: parseInt(data['#']),
+                name: data['City'],
+                telecomTowers: parseInt(data['Telecom\nTowers']),
+            }));
 
-            response.json(data);
+            response.json(cities);
         } catch (e) {
             return this.handleError(next, new RequestException(INTERNAL_SERVER_ERROR, ['unable to get city data']));
         }
